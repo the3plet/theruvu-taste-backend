b12f3f988c4661c9b187701c9ed51ab7cca72b7f
Srj <soorajp2002@gmail.com>
2025-08-11 16:25:46 +0530

    added desciption to fooditem

---
+++ prisma/migrations/20250811105238_added/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "FoodItem" ADD COLUMN     "description" TEXT NOT NULL DEFAULT 'No description provided';
--- src/modules/fooditem/dto/fooditem.dto.ts
+++ src/modules/fooditem/dto/fooditem.dto.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const foodItemSchema = z.object({
   name: z.string().min(1, "Name is required"),
   price: z.number().positive("Price must be a positive number"),
+  description:z.string().nonempty('Desciption is required'),
   foodSpotId: z.string().uuid("Invalid FoodSpot ID"),
 });
 
